94e7314386326074aaded222b3bc328e3bcb7754
creha3 <151726411+creha3@users.noreply.github.com>
2024-03-27 17:51:58 +0900

    Delete profile directory

--- profile/main.js
+++
@@ -1,7 +0,0 @@
-$(".site-header-navbtn").on("click", () => {
-    $("body").toggleClass("is-nav-open");
-});
-
-$(".site-header-nav").on("click", "a", () => {
-    $("body").removeClass("is-nav-open")
-})
